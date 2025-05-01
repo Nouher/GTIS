ddb5932b693f1c8e0ca44b036ec0298bde51c2c5
unknown <nouhirh@gmail.com>
2025-05-01 15:36:41 +0100

    navbar

--- app/page.tsx
+++ app/page.tsx
@@ -3,7 +3,6 @@ import bg from '../public/bg2.jpg'
 import img1 from '../public/bg1.jpg'
 import Wave from "@/Components/Wave";
 import { ArrowRight } from "lucide-react";
-import { Button } from "@/Components/ui/button";
 
 
 export default function Home() {
