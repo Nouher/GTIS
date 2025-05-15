2d2962150dd40f9ee3543cafce3176faafa0b07c
unknown <nouhirh@gmail.com>
2025-05-15 17:49:15 +0100

    section 2 home page

--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,7 @@
 'use client'
 import Image from "next/image";
-import bg from '../public/bg4.jpg'
 import img1 from '../public/bg1.jpg'
-import { ArrowRight } from "lucide-react";
+import { ArrowRight, CalendarSyncIcon, ConstructionIcon, OrbitIcon, WrenchIcon } from "lucide-react";
 import { Typewriter } from "react-simple-typewriter";
 
 
@@ -12,12 +11,17 @@ export default function Home() {
       <div className=" w-full">
         <section className="relative h-screen w-full overflow-hidden">
           <div className="w-full h-full bg-amber-200">
-            <Image src={bg} alt="silos" className="move w-full" />
-            {/* <video src="/vd1.mp4" className="absolute top-0 left-0 w-full h-full object-cover"/> */}
+            {/* <Image src={bg} alt="silos" className="move w-full" /> */}
+            <video src="/vd1.mp4" className="absolute top-0 left-0 w-full h-full object-cover" 
+            autoPlay
+            loop
+            muted
+            playsInline />
+
           </div>
           <div className="h-screen absolute top-0 w-full ">
             <div className=" bg-[#1128479d] w-3/4 px-20 flex flex-col  h-full [clip-path:polygon(0_0,100%_0,80%_100%,0%_100%)] absolute top-0">
-              <p className="h-72 text-white text-2xl font-semibold max-w-[1000px]  pl-4 mt-72 leading-16 pb-10">
+              <p className="h-72 text-white text-2xl font-semibold max-w-[1000px] pl-4 mt-86 leading-14 pb-10">
                 <span className="text-blue-400 font-bold ">
                   GTIS 
                 </span>   
@@ -44,7 +48,26 @@ export default function Home() {
         <div className="bg-gray-100 w-full h-40 flex justify-center items-center text-xl text-center px-20 text-[#112847]">
             <p>GTIS offers complete solutions for optimizing and maintaining flour mills, including the installation of automated systems, preventive and corrective maintenance of production equipment, and the supply of spare parts specific to flour production lines.</p>
         </div>
-        <section className="h-screen"></section>
+        <section className="h-[500px] flex items-center">
+          <div className="w-full flex justify-around items-center font-bold text-2xl px-40 text-[#112847]">
+            <div className="flex flex-col justify-center items-center w-68 text-center">
+              <CalendarSyncIcon size={110} color="#112847"/>
+              <h2 className="pt-5">Mechanical &amp; Electrical Assembly</h2>
+            </div>
+            <div className="flex flex-col justify-center items-center w-68 text-center">
+              <WrenchIcon size={110} color="#112847"/>
+              <h2 className="pt-5">Automation Solutions</h2>
+            </div>
+            <div className="flex flex-col justify-center items-center w-68 text-center">
+              <ConstructionIcon size={110} color="#112847"/>
+              <h2 className="pt-5"> Preventive &amp; Corrective Maintenance</h2>
+            </div>
+            <div className="flex flex-col justify-center items-center w-68 text-center">
+              <OrbitIcon size={110} color="#112847"/>
+              <h2 className="pt-5">Spare Parts Supply</h2>
+            </div>
+          </div>
+        </section>
       </div>
     </div>
   );
